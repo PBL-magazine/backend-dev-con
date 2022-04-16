67eb8bc18ccca0a7ba30331edf489bf1317a8ba9
reload1bronze <nature9th@gmail.com>
2022-04-16 18:11:35 +0900

    [추가] 로그인 된경우 API 요청시 400 응답 - 로그인된 사용자는 로그인, 회원가입 API 요청 차단

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const userRouter = express.Router();
-const protectorMiddleware = require("../middlewares/protectorMiddleware");
+const {
+  protectorMiddleware,
+  notSigninMiddleware,
+} = require("../middlewares/protectorMiddleware");
 const {
   signupValidator,
   signinValidator,
@@ -8,13 +11,13 @@ const {
 
 const { signup, signin } = require("../controllers/userController");
 
-userRouter.post("/signup", signupValidator, signup);
-userRouter.post("/signin", signinValidator, signin);
+userRouter.post("/signup", notSigninMiddleware, signupValidator, signup);
+userRouter.post("/signin", notSigninMiddleware, signinValidator, signin);
 
 // TODO: user 받아오는지 확인 => 삭제할 것
 userRouter.get("/me", protectorMiddleware, async (req, res) => {
   const { user } = res.locals;
-  console.log(user);
+  console.log(`/me 유저 확인: ${user.user_id}`);
   return res.status(400).send({ user });
 });
 

513b663881e2f11683e4399bce23fa60ab765e66
reload1bronze <nature9th@gmail.com>
2022-04-16 18:06:51 +0900

    [추가] 프로텍터 미들웨어 작성 - JWT발급(로그인) 여부에 따른 API 요청 접수/거부

--- src/middlewares/protectorMiddleware.js
+++ src/middlewares/protectorMiddleware.js
@@ -29,4 +29,19 @@ const protectorMiddleware = (req, res, next) => {
   }
 };
 
-module.exports = protectorMiddleware;
+const notSigninMiddleware = (req, res, next) => {
+  const { authorization } = req.headers;
+  const [authType, authToken] = (authorization || "").split(" ");
+
+  if (!authToken || authType !== "Bearer") {
+    // 로그인 확인 불가 => 통과
+    return next();
+  } else {
+    // 토큰 있으면 로그인된 것으로 판단
+    return res
+      .status(400)
+      .json({ ok: false, message: "이미 로그인이 되었습니다." });
+  }
+};
+
+module.exports = { protectorMiddleware, notSigninMiddleware };

6796ac7dcd68f8e0437ed7fc3c2d676873f38033
reload1bronze <nature9th@gmail.com>
2022-04-19 16:31:12 +0900

    [추가] /api/users/auth 라우터 추가 - 유저 데이터 생성 확인을 위한 기능

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,5 +1,8 @@
 const express = require("express");
-const { notSigninMiddleware } = require("../middlewares/protectorMiddleware");
+const {
+  notSigninMiddleware,
+  protectorMiddleware,
+} = require("../middlewares/protectorMiddleware");
 const {
   signupValidator,
   signinValidator,
@@ -11,5 +14,10 @@ const userRouter = express.Router();
 // TODO: [요구사항 3-3] 로그인한 사용자가 회원가입 또는 로그인 페이지 접근시 예외처리 (notSigninMiddleware 미들웨어 적용했음)
 userRouter.post("/signup", notSigninMiddleware, signupValidator, signup);
 userRouter.post("/signin", notSigninMiddleware, signinValidator, signin);
+
+userRouter.get("/auth", protectorMiddleware, async (req, res) => {
+  const { user } = res.locals;
+  return res.status(200).send({ user });
+});
 
 module.exports = userRouter;

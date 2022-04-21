c3bbe73c1f108855eb217275fc246e15efb9da2f
reload1bronze <nature9th@gmail.com>
2022-04-21 15:59:04 +0900

    [수정] Joi 메시지 작성 - Joi 기본 메시지 대신 출력할 메시지 입력

--- src/middlewares/validatorMiddleware.js
+++ src/middlewares/validatorMiddleware.js
@@ -12,15 +12,31 @@ const signupValidator = (req, res, next) => {
     checkPassIncludesNick(password);
   } catch (error) {
     // 비밀번호를 잘못 요청한 경우 => 400 Bad Request
-    return res.status(400).send({ ok: false, message: error.message });
+    return res.status(400).json({ ok: false, message: error.message });
   }
 
   const signupSchema = Joi.object({
-    email: Joi.string().email().max(30).required(),
+    email: Joi.string().email().max(30).required().messages({
+      "any.required": "이메일을 입력해 주세요.",
+      "string.empty": "이메일을 입력해 주세요.",
+      "string.email": "이메일 형식을 지켜주세요.",
+      "string.max": "이메일은 최대 30자 입력해 주세요.",
+    }),
     // TODO: [요구사항 1-1] 최소 3자 이상, 알파벳 대소문자(a~z, A~Z), 숫자(0~9)
-    nickname: Joi.string().alphanum().min(3).max(30).required(),
+    nickname: Joi.string().alphanum().min(3).max(30).required().messages({
+      "any.required": "닉네임을 입력해 주세요.",
+      "string.empty": "닉네임을 입력해 주세요.",
+      "string.alphanum": "닉네임은 알파벳 대소문자, 숫자만 가능해요.",
+      "string.min": "닉네임은 최소 3자 이상 입력해 주세요.",
+      "string.max": "닉네임은 최대 30자 까지 입력해 주세요.",
+    }),
     // TODO: [요구사항 1-2] 최소 4자이상, (닉네임과 같은 값이 포함된 경우 회원가입에 실패) => 별도 처리
-    password: Joi.string().min(4).max(200).required(),
+    password: Joi.string().min(4).max(20).required().messages({
+      "any.required": "비밀번호를 입력해 주세요.",
+      "string.empty": "비밀번호를 입력해 주세요.",
+      "string.min": "비밀번호는 최소 4자 이상 입력해 주세요",
+      "string.max": "비밀번호는 최대 20자 까지 입력해 주세요",
+    }),
     role: Joi.number(),
     // TODO: [요구사항 1-3] 비밀번호 확인 => 프런트에서 처리하기로 협의
   });
@@ -33,7 +49,7 @@ const signupValidator = (req, res, next) => {
 
   if (error) {
     // 이메일, 닉네임, 비밀번호를 잘못 요청한 경우 => 400 Bad Request
-    return res.status(400).send({ ok: false, message: error.message });
+    return res.status(400).json({ ok: false, message: error.message });
   } else {
     req.body = value;
     next();
@@ -55,7 +71,7 @@ const signinValidator = (req, res, next) => {
 
   if (error) {
     // 이메일, 비밀번호를 잘못 요청한 경우 => 400 Bad Request
-    return res.status(400).send({ ok: false, message: error.message });
+    return res.status(400).json({ ok: false, message: error.message });
   } else {
     req.body = value;
     next();
@@ -73,7 +89,7 @@ const postValidator = (req, res, next) => {
 
   if (error) {
     // 검증 불합격시 => 400 Bad Request
-    return res.status(400).send({ ok: false, message: error.message });
+    return res.status(400).json({ ok: false, message: error.message });
   } else {
     req.body = value;
     next();
@@ -90,7 +106,7 @@ const commentValidator = (req, res, next) => {
 
   if (error) {
     // 내용이 string이 아닌경우 => 400 Bad Request (string이 아닌경우가 있나?)
-    return res.status(400).send({ ok: false, message: error.message });
+    return res.status(400).json({ ok: false, message: error.message });
   } else {
     req.body = value;
     next();
